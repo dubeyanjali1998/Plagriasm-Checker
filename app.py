994159f72cd10d8e66a85750e4b717a8fa8af4e5
dubeyanjali1998 <76435586+dubeyanjali1998@users.noreply.github.com>
2023-02-10 14:28:19 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 #pip install -U scikit-learn
 #Make sure all the .txt files that need to be checked are in the same directory as the script
 import os
-from sklearn.feature_extraction.text import TfidfVectorizer
+from text.py import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 user_files = [doc for doc in os.listdir() if doc.endswith('.txt')]
